566d75336c2532338508522699c859ee006e91c7
iosmkarim <iosmkarim@gmail.com>
2020-10-04 17:03:16 -0400

    adding my starter proj

---
+++ StarterProj/StarterProj/StarterProj.h
@@ -0,0 +1,19 @@
+//
+//  StarterProj.h
+//  StarterProj
+//
+//  Created by MDREZAUL KARIM on 10/4/20.
+//  Copyright © 2020 storage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StarterProj.
+FOUNDATION_EXPORT double StarterProjVersionNumber;
+
+//! Project version string for StarterProj.
+FOUNDATION_EXPORT const unsigned char StarterProjVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StarterProj/PublicHeader.h>
+
+
---
+++ StarterProj/StarterProj/Validator.swift
@@ -0,0 +1,15 @@
+//
+//  Validator.swift
+//  StarterProj
+//
+//  Created by MDREZAUL KARIM on 10/4/20.
+//  Copyright © 2020 storage. All rights reserved.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func sayHello() {
+        print("My name is ....")
+    }
+}
